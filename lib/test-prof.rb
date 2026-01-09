162207f746812a5115b8d7c2636a4ed4cc58bab2
jamiecobbett <jcobbett@gocardless.com>
2026-01-09 17:57:19 +0000

    Fix RuboCop plugin loading by removing conditional autoload
    The conditional 'if defined?(RuboCop)' check prevented the autoload from
    being set up when RuboCop's plugin loader required 'test-prof'. This is
    because:
    
    1. RuboCop's plugin loader requires 'test-prof' (the gem name with hyphen)
    2. This loads lib/test-prof.rb before RuboCop is fully loaded
    3. The defined?(RuboCop) check fails, so autoload is never set up
    4. RuboCop then tries to access RuboCop::TestProf::Plugin from gemspec
    5. This fails with 'uninitialized constant RuboCop::TestProf'
    
    The solution is to unconditionally set up the autoload in lib/test-prof.rb.
    This is safe because:
    - This file (with hyphen) is primarily loaded by RuboCop's plugin system
    - The autoload only triggers when RuboCop::TestProf is accessed
    - lib/rubocop/test_prof.rb handles the case when RuboCop is not installed
    - Non-RuboCop users who 'require test_prof' use the underscore version
    
    Fixes #331

--- lib/test-prof.rb
+++ lib/test-prof.rb
@@ -3,8 +3,12 @@
 require "test_prof"
 
 # For RuboCop plugin
-if defined?(RuboCop)
-  module RuboCop
-    autoload :TestProf, "rubocop/test_prof"
-  end
+# This file (test-prof.rb with hyphen) is only loaded when RuboCop loads
+# test-prof as a plugin. We need to set up the autoload unconditionally here
+# because RuboCop's plugin loader will try to access RuboCop::TestProf::Plugin
+# from gemspec metadata before the RuboCop constant is fully defined.
+# Since this file is only loaded in RuboCop plugin context, it's safe to
+# define the RuboCop module here.
+module RuboCop
+  autoload :TestProf, "rubocop/test_prof"
 end
